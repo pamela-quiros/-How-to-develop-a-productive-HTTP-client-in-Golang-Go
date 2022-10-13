7b10f20f793c471f23121d3252d95eeabc1742b8
Pamela Quirós Ureña <pamelaquirosu@hotmail.com>
2022-10-13 13:57:01 -0600

    Section 3: Testing

---
+++ gohttp/client_core_test.go
@@ -0,0 +1,90 @@
+package gohttp
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestGetRequestHeaders(t *testing.T) {
+	//Initialization
+	client := httpClient{}
+	commonHeaders := make(http.Header)
+	commonHeaders.Set("Content-Type", "application/json")
+	commonHeaders.Set("User-Agent", "cool-http-client")
+	client.Headers = commonHeaders
+
+	//Execution
+	requestHeaders := make(http.Header)
+	requestHeaders.Set("X-Request-Id", "ABC-123")
+
+	finalHeaders := client.getRequestHeaders(requestHeaders)
+
+	//Validation
+	if len(finalHeaders) != 3 {
+		t.Error("we expect 3 headers")
+	}
+
+	if finalHeaders.Get("X-Request-Id") != "ABC-123" {
+		t.Error("invalid request id received")
+	}
+
+	if finalHeaders.Get("Content-Type") != "application/json" {
+		t.Error("invalid content type received")
+	}
+
+	if finalHeaders.Get("User-Agent") != "cool-http-client" {
+		t.Error("invalid user agent received")
+	}
+}
+
+func TestGetRequestBody(t *testing.T) {
+	//Initialization
+	client := httpClient{}
+
+	t.Run("noBodyNilResponse", func(t *testing.T) {
+		//Execution
+		body, err := client.getRequestBody("", nil)
+
+		//Validation
+		if err != nil {
+			t.Error("no error expected when passing a nil body")
+		}
+
+		if body != nil {
+			t.Error("no body expected when passing a nil body")
+		}
+	})
+
+	t.Run("BodyWithJson", func(t *testing.T) {
+		//Execution
+		requestBody := []string{"one", "two"}
+		body, err := client.getRequestBody("application/json", requestBody)
+		fmt.Println(err)
+		fmt.Println(string(body))
+
+		//Validation
+		if err != nil {
+			t.Error("no error expected when marshaling slice as json")
+		}
+
+		if string(body) != `["one","two"]` {
+			t.Error("invalid json body obtained")
+		}
+	})
+
+	t.Run("BodyWithXml", func(t *testing.T) {
+		//Execution
+
+		//Validation
+
+	})
+
+	t.Run("BodyWithJsonAsDefault", func(t *testing.T) {
+		//Execution
+
+		//Validation
+
+	})
+
+}
